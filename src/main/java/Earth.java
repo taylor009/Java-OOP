24e07cd092e482d53932b91fb611f53de39800a3
taylor <taylor@affair.dev>
2020-07-01 12:17:25 -0400

    First Commit

---
+++ src/main/java/Earth.java
@@ -0,0 +1,15 @@
+public class Earth {
+    public static void main(String[] args) {
+        Human tom;
+
+        tom = new Human();
+
+        tom.age = 5;
+        tom.eyeColor = "Brown";
+        tom.heightInInches = 72;
+        tom.name = "Tom Zsabo";
+
+        tom.speak();
+
+    }
+}
---
+++ src/main/java/Human.java
@@ -0,0 +1,25 @@
+public class Human {
+    String name;
+    int age;
+    int heightInInches;
+    String eyeColor;
+
+    public void speak() {
+        System.out.println("Hello my name is " + name);
+        System.out.println("I am " + heightInInches + " inches tall");
+        System.out.println("I am " + age + " years old");
+        System.out.println("My eye color is " + eyeColor);
+    }
+
+    public void eat() {
+        System.out.println("eating....");
+    }
+
+    public void walk() {
+        System.out.println("walking...");
+    }
+
+    public void work() {
+        System.out.println("working...");
+    }
+}
